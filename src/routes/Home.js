6f0ccddd090acf7101b70710061a1a61e0a52bac
ksnghyk <hani103201@naver.com>
2023-05-03 20:24:50 +0900

    tweet 화면에 보이게 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { dbService } from "../fbase";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async() => {
+    const dbTweets = await dbService.collection("tweet").get();
+    dbTweets.forEach(document => {
+        const tweetObject = {
+            ...document.data(),
+            id: document.id,
+        };
+
+        setTweets(prev => [tweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
+
   const onSubmit = async(event) => {
     event.preventDefault();
     await dbService.collection('tweet').add({
@@ -28,6 +44,13 @@ const Home = () => {
         />
         <input type="submit" value="Tweet" />
       </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
